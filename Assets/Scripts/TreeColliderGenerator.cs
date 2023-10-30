1d287b4f5560bb06f5a14a849bf76ebd9e61371b
Zixiang Xu <1250105753@qq.com>
2023-10-30 17:32:50 -0400

    Protagonist_Fixing

--- Assets/Scripts/TreeColliderGenerator.cs
+++ Assets/Scripts/TreeColliderGenerator.cs
@@ -13,8 +13,7 @@ public class TreeColliderGenerator : MonoBehaviour {
                 int treeType = tree.prototypeIndex; // gain prototypeIndex of each tree
                 Vector3 treeWorldPos = Vector3.Scale(tree.position, terrain.terrainData.size) + terrain.transform.position;
 
-                if (treeType >= 0 && treeType < treeColliderPrefabs.Length)
-                {
+                if (treeType >= 0 && treeType < treeColliderPrefabs.Length) {
                     GameObject treeColliderPrefab = treeColliderPrefabs[treeType];
                     GameObject treeCollider = Instantiate(treeColliderPrefab, treeWorldPos, Quaternion.identity);
                     treeCollider.layer = LayerMask.NameToLayer("Trees");
